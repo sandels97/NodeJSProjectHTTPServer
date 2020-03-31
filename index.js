6e0e733b8ed876ea7f684659647586abcdeaa0e5
Santtu Hyvärinen <santtu.hyvarinen@kuori.tech>
2020-03-31 12:06:25 +0300

    Assignment done

--- index.js
+++ index.js
@@ -29,5 +29,44 @@ app.get("/api/exercise", function (req, res) {
 	res.status(200).end();
 })
 
+app.post("/api/exercise", function (req, res) {
+	
+	res.setHeader('Content-Type', 'text/html');
+	
+	var result = "<h1>Hello from Express!</h1><h2>POST parameters</h2><p>I received these parameters: </p><ul>";
+	
+	
+	var response = {};
+	for (var propName in req.body) {
+		result += "<li>" + propName + ": " + req.body[propName] + "</li>";
+	}
+	
+	result += "</ul>";
+	
+	res.status(200).send(result);
+})
+
+app.post("/api/login", function (req, res) {
+	
+	res.setHeader('Content-Type', 'application/json');
+	
+	if(!req.body["user"] || !req.body["pwd"]) {
+		res.status(400).end();
+		return;
+	}
+	
+	if(req.body["user"] == "mark" && req.body["pwd"] == "giraffe") {
+		var response = {};
+		
+		response["user"] = req.body["user"];
+		
+		res.write(JSON.stringify(response));
+		res.end();
+		return;
+	}
+	
+	res.status(403).end();
+})
+
 app.listen(port, () => 
 console.log(`Express app listening on port ${port}!`))
